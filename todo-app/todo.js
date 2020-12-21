8a6aba3ebd2db48091464d083ce5d9d486bc3c5a
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2020-12-20 23:44:13 -0800

    vanilla js practice

--- todo-app/todo.js
+++ todo-app/todo.js
@@ -1,7 +1,47 @@
-const the = document.querySelectorAll("p")
+const todos = [{
+    text: 'Make Dinner',
+    completed: true
+},
 
-the.forEach(function(p) {
-    if(p.textContent.includes('the')) {
-        p.remove()
-    }
+{
+    text: 'Clean Dishes',
+    completed: false
+},
+
+{
+    text: 'Wash car',
+    completed: true
+},
+
+{
+    text: 'Fold Laundry',
+    completed: false
+},
+
+{
+    text: 'Finish Homework',
+    completed: true
+}]
+
+// const ps = document.querySelectorAll("p")
+
+// ps.forEach(function(p) {
+//     if(p.textContent.includes('the')) {
+//         p.remove()
+//     }
+// })
+
+const incompleteTodos = todos.filter(function(todo) {
+    return !todo.completed
+})
+
+const summary = document.createElement('h2')
+summary.textContent = `you have ${incompleteTodos.length} left todo`
+document.querySelector('body').appendChild(summary)
+
+
+todos.forEach(function(todo) {
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('body').appendChild(p)
 })
