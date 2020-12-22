ec808ff64f6e122927bf25adb3a8a07b97643ec5
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2020-12-21 23:21:52 -0800

    advanced queries

--- todo-app/todo.js
+++ todo-app/todo.js
@@ -31,17 +31,23 @@ const todos = [{
 //     }
 // })
 
-const incompleteTodos = todos.filter(function(todo) {
+const incompleteTodos = todos.filter(function(todo){
     return !todo.completed
 })
 
+
 const summary = document.createElement('h2')
-summary.textContent = `you have ${incompleteTodos.length} left todo`
+summary.textContent = `You have ${incompleteTodos.length} left`
 document.querySelector('body').appendChild(summary)
 
 
-todos.forEach(function(todo) {
+todos.forEach(function(p) {
     const p = document.createElement('p')
-    p.textContent = todo.text
+    p.textContent = todos.text
     document.querySelector('body').appendChild(p)
 })
+
+document.querySelector('#remove-todo').addEventListener('click', function(){
+    console.log('Message removed')
+})
+
