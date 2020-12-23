44c8c876089487118ffc83caadcf387f1f5e7296
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2020-12-22 23:11:12 -0800

    live data

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -15,6 +15,24 @@ const notes = [ {
 //     e.target.textContent = 'new todo'
 // })
 
+const filters = {
+    searchText: ''
+}
+
+renderNotes = function(notes, filters) {
+    const filteredNotes = notes.filter(function(note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    filteredNotes.forEach(function(note) {
+        const noteElement = document.createElement('p')
+        noteElement.textContent = note.title
+        document.querySelector('body').appendChild(noteElement)
+    })
+}
+
+renderNotes(notes, filters)
+
+
 const newButton = document.createElement('button')
 
 newButton.textContent = 'add new todo'
@@ -24,6 +42,13 @@ document.querySelector('body').appendChild(newButton)
 document.querySelector('button').addEventListener('click', function(e) {
     e.target.textContent = 'new todo added'
 })
+
+document.querySelector('#search-text').addEventListener('input', function(e) {
+    filters.searchText = e.target.value 
+    renderNotes(notes, filters)
+})
+
+
 
 
 
