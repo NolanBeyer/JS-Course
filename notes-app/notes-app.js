2415fc53cc0737f81dd132f2a50ebd2965477b1c
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-04-20 01:03:01 -0700

    advanced querie practice

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,14 +1,48 @@
+const notes = [
+  {
+    title: 'My next trip',
+    body: 'Somewhere to fix my body',
+  },
+  {
+    title: 'Habbits to work on',
+    body: 'Sleep schedule',
+  },
+  {
+    title: 'Office modifications',
+    body: 'Get a good chair',
+  },
+]
+
+document.querySelector('#create-note').addEventListener('click', function (e) {
+  e.target.textContent = 'The button was clicked'
+})
+
+document.querySelector('#remove-all').addEventListener('click', function (e) {
+  e.target.textContent = 'Notes deleted'
+})
+
+document.querySelectorAll('.note').forEach(function (note) {
+  note.remove()
+})
+
 //* DOM - Document Object Model
 
 //* Query and remove
-const p = document.querySelector('p')
-// p.remove()
+/// p.remove()
 
 //* Query all and remove
-const ps = document.querySelectorAll('p')
+// const ps = document.querySelectorAll('p')
 
-ps.forEach(function (p) {
-  p.textContent = '****'
-  //   console.log(p.textContent)
-  //   p.remove()
-})
+// ps.forEach(function (p) {
+//   p.textContent = '****'
+//   console.log(p.textContent)
+//   p.remove()
+// })
+
+//* Create a new element
+
+// const newElement = document.createElement('p')
+
+// newElement.textContent = 'This is a new element from javascript'
+
+// document.querySelector('body').appendChild(newElement)
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,7 +1,50 @@
-const paragraphs = document.querySelectorAll('p')
+const todos = [
+  {
+    text: 'Make Dinner',
+    completed: true,
+  },
+  {
+    text: 'Clean Dishes',
+    completed: false,
+  },
+  {
+    text: 'Wash car',
+    completed: true,
+  },
+  {
+    text: 'Fold Laundry',
+    completed: false,
+  },
+  {
+    text: 'Finish Homework',
+    completed: true,
+  },
+]
 
-paragraphs.forEach(function (paragraph) {
-  if (paragraph.textContent.includes('the')) {
-    paragraph.remove()
-  }
+document.querySelector('button').addEventListener('click', function (e) {
+  e.target.textContent = 'Todo added'
 })
+
+document.querySelector('#add-todo').addEventListener('click', function (e) {
+  e.target.textContent = 'todo added'
+})
+
+//* You have two todos left
+
+// const incompleteTodos = todos.filter((todo) => !todo.completed)
+// const summary = createElement('h2')
+// summary.textContent = `You have${incompleteTodos.length} todos left to finish`
+// document.querySelector('body').appendChild(summary)
+
+// todos.forEach(function (todo) {
+//   const p = document.createElement('h2')
+//   p.textContent = todo.text
+//   document.querySelector('body').appendChild(p)
+// })
+// const paragraphs = document.querySelectorAll('p')
+
+// paragraphs.forEach(function (paragraph) {
+//   if (paragraph.textContent.includes('the')) {
+//     paragraph.remove()
+//   }
+// })
